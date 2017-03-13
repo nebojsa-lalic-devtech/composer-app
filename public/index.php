a43dba8be234154865ffd79ede7d348b2d79851a
nebojsa.lalic <nebojsa.lalic@devtechgroup.com>
2017-03-13 08:48:00 +0100

    basic routes with Klein implemented

--- public/index.php
+++ public/index.php
@@ -1,46 +1,15 @@
 <?php
 
-//require_once '../app/NL/Autoload/autoloader.php';
 require_once '../vendor/autoload.php';
 
 use Klein\Klein;
-//use app\NL\Route\FrontController;
 use app\NL\Models\Employee\Employee;
 use app\NL\Models\Company\Company;
 use app\NL\Models\Project\Project;
 
 $klein = new Klein();
 
-
-
 $klein->respond('GET', '/', function () {
-    return 'Klein TEST HOME radi!';
-
-//    $user = new Employee('Nebojsa', 'Lalic', 'nebojsa.lalic@devtechgroup.com', 'Software developer');
-//    $user2 = new Employee('Petar', 'Petrovic', 'petarpetrovic@gmail.com', 'QA');
-//    $user3 = new Employee('X', 'Man', 'xman@yahoo.com', 'Project manager');
-//
-//    echo 'Welcome to <b>Home Page</b> :)!<br>';
-//    echo '-------------------------------------------------------';
-//    echo '<h2>Hello from autoloaded class("Employee") with next details:</h2><br>';
-//    echo '<p>' . $user->getUserDetails() . '</p>';
-//    echo '<br>' . '<p>' . $user2->getUserDetails() . '</p>';
-//    echo '<br>' . '<p>' . $user3->getUserDetails() . '</p>';
-
-});
-
-$klein->respond('GET', '/klein', function () {
-    return 'Klein TEST "/klein" radi!';
-});
-
-$klein->dispatch();
-
-/*
-$frontController = new FrontController();
-
-$frontController->getUri();
-
-$frontController->setRoute('/', function () {
     $user = new Employee('Nebojsa', 'Lalic', 'nebojsa.lalic@devtechgroup.com', 'Software developer');
     $user2 = new Employee('Petar', 'Petrovic', 'petarpetrovic@gmail.com', 'QA');
     $user3 = new Employee('X', 'Man', 'xman@yahoo.com', 'Project manager');
@@ -53,7 +22,7 @@ $frontController->setRoute('/', function () {
     echo '<br>' . '<p>' . $user3->getUserDetails() . '</p>';
 });
 
-$frontController->setRoute('/about', function () {
+$klein->respond('GET', '/about', function () {
     $company = new Company('DevTech', array('Mihajla Pupina 12', 'Janka Cmelika 7'), 'Information Technology');
 
     echo 'If you want <b>about page</b>, you are right there! :)<br>';
@@ -62,11 +31,10 @@ $frontController->setRoute('/about', function () {
     echo '<p>' . $company->getCompanyDetails() . '</p>';
 });
 
-$frontController->setRoute('/project', function () {
+$klein->respond('GET', '/project', function () {
     $project = new Project('AppRiver', 'In Progress', 'Google', 'Street 01', 'IT');
 
     echo $project->getProject();
 });
 
-$frontController->dispatch();
-*/
+$klein->dispatch();
