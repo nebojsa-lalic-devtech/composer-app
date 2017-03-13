2887371017875e989ca48b061a522766102f2b08
nebojsa.lalic <nebojsa.lalic@devtechgroup.com>
2017-03-13 15:18:36 +0100

    added 'Smarty template' views

--- public/index.php
+++ public/index.php
@@ -10,31 +10,50 @@ use app\NL\Models\Project\Project;
 $klein = new Klein();
 
 $klein->respond('GET', '/', function () {
+    $smarty = new Smarty();
+
     $user = new Employee('Nebojsa', 'Lalic', 'nebojsa.lalic@devtechgroup.com', 'Software developer');
     $user2 = new Employee('Petar', 'Petrovic', 'petarpetrovic@gmail.com', 'QA');
     $user3 = new Employee('X', 'Man', 'xman@yahoo.com', 'Project manager');
 
-    echo 'Welcome to <b>Home Page</b> :)!<br>';
-    echo '-------------------------------------------------------';
-    echo '<h2>Hello from autoloaded class("Employee") with next details:</h2><br>';
-    echo '<p>' . $user->getUserDetails() . '</p>';
-    echo '<br>' . '<p>' . $user2->getUserDetails() . '</p>';
-    echo '<br>' . '<p>' . $user3->getUserDetails() . '</p>';
+    $userDetails = $user->getUserDetails();
+    $user2Details = $user2->getUserDetails();
+    $user3Details = $user3->getUserDetails();
+
+    $smarty->assign('userDetails', $userDetails);
+    $smarty->assign('user2Details', $user2Details);
+    $smarty->assign('user3Details', $user3Details);
+
+    $indexView = $smarty->display('index.tpl');
+    return $indexView;
 });
 
 $klein->respond('GET', '/about', function () {
+    $smarty = new Smarty();
+
     $company = new Company('DevTech', array('Mihajla Pupina 12', 'Janka Cmelika 7'), 'Information Technology');
 
-    echo 'If you want <b>about page</b>, you are right there! :)<br>';
-    echo '-------------------------------------------------------';
-    echo '<h2>This is page about company:</h2><br>';
-    echo '<p>' . $company->getCompanyDetails() . '</p>';
+    $companyDetails = $company->getCompanyDetails();
+
+    $smarty->assign('companyDetails', $companyDetails);
+
+    $aboutView = $smarty->display('about.tpl');
+
+    return $aboutView;
 });
 
 $klein->respond('GET', '/project', function () {
+    $smarty = new Smarty();
+    
     $project = new Project('AppRiver', 'In Progress', 'Google', 'Street 01', 'IT');
 
-    echo $project->getProject();
+    $projectDetails = $project->getProject();
+    
+    $smarty->assign('projectDetails', $projectDetails);
+
+    $projectView = $smarty->display('project.tpl');
+    
+    return $projectView;
 });
 
 $klein->dispatch();
