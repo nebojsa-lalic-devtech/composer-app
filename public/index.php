db0b59be84f2cc6ff2a1d620661cca940f96f2cc
nebojsa.lalic <nebojsa.lalic@devtechgroup.com>
2017-03-14 08:54:19 +0100

    code formating

--- public/index.php
+++ public/index.php
@@ -12,7 +12,6 @@ $klein = new Klein();
 $smarty = new Smarty();
 
 $klein->respond('GET', '/', function () use ($smarty) {
-
     $user = new Employee('Nebojsa', 'Lalic', 'nebojsa.lalic@devtechgroup.com', 'Software developer');
     $user2 = new Employee('Petar', 'Petrovic', 'petarpetrovic@gmail.com', 'QA');
     $user3 = new Employee('X', 'Man', 'xman@yahoo.com', 'Project manager');
@@ -30,7 +29,6 @@ $klein->respond('GET', '/', function () use ($smarty) {
 });
 
 $klein->respond('GET', '/about', function () use ($smarty) {
-
     $company = new Company('DevTech', array('Mihajla Pupina 12', 'Janka Cmelika 7'), 'Information Technology');
 
     $companyDetails = $company->getCompanyDetails();
@@ -43,20 +41,18 @@ $klein->respond('GET', '/about', function () use ($smarty) {
 });
 
 $klein->respond('GET', '/project', function () use ($smarty) {
-    
     $project = new Project('AppRiver', 'In Progress', 'Google', 'Street 01', 'IT');
 
     $projectDetails = $project->getProject();
-    
+
     $smarty->assign('projectDetails', $projectDetails);
 
     $projectView = $smarty->display('templates/project.tpl');
-    
+
     return $projectView;
 });
 
 $klein->onHttpError(function () use ($smarty) {
-
     $current_uri = $_SERVER['REQUEST_URI'];
 
     $errorMessage = "Oooups...your URL: \"" . $current_uri . "\" does not exist!";
